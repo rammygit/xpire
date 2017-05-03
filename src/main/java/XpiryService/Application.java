cfbba382ff478d81f5118b9f61d4d25703a691f9
ramk <rammyramkumar@gmail.com>
2017-05-02 22:28:23 -0400

    Initial commit

---
+++ src/main/java/XpiryService/Application.java
@@ -0,0 +1,21 @@
+package XpiryService;
+
+import static spark.Spark.get;
+import static spark.Spark.port;
+
+/**
+ * Created by ramkumarsundarajan on 5/2/17.
+ */
+public class Application {
+
+    public static void main(String args[]) {
+
+        port(8000);
+
+        get("/hello", (req, res) -> "Hello World");
+
+
+
+
+    }
+}
